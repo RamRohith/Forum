0798923784720a74decb133a4d8c81579a18d784
RamRohith <bolisettyramrohith99@gmail.com>
2019-03-13 16:01:11 +0530

    This is the first commit

---
+++ forum_environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ram\desktop\python intern\practicing all programs\forum_project\forum_environment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
